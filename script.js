3bdf3c3736fc79095ab956f0fb469d75bbfaa966
Ambyjkl <ambareeshbalaji@gmail.com>
2016-09-27 16:47:34 -0400

    Change layout and add new links

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 "use strict";
-function changeBackground(){
+
+function changeBackground() {
     var today = new Date();
-    var s = today.getSeconds() + today.getMilliseconds()/1000;
-    var m = today.getMinutes() + s/60;
-    var h = today.getHours()+ m/60;
-    var r = Math.trunc((h < 12) ? (h *256/12) : ((24-h) *256/12));
+    var s = today.getSeconds() + today.getMilliseconds() / 1000;
+    var m = today.getMinutes() + s / 60;
+    var h = today.getHours() + m / 60;
+    var r = Math.trunc((h < 12) ? (h * 256 / 12) : ((24 - h) * 256 / 12));
     var R = 255 - r;
-    var g = Math.trunc((m < 30) ? (m *256/30) : ((60-m) *256/30));
+    var g = Math.trunc((m < 30) ? (m * 256 / 30) : ((60 - m) * 256 / 30));
     var G = 255 - g;
-    var b = Math.trunc((s < 30) ? (s *256/30) : ((60-s) *256/30));
+    var b = Math.trunc((s < 30) ? (s * 256 / 30) : ((60 - s) * 256 / 30));
     var B = 255 - b;
-    var rr=Math.trunc(Math.pow(r*R,0.5));
-    var gg=Math.trunc(Math.pow(g*G,0.5));
-    var bb=Math.trunc(Math.pow(b*B,0.5));
-    $("body").css({'background-image': `linear-gradient(to top,  rgb(${r},${g},${b}) 1%, rgb(${R},${G},${B}) 100%)`});
-    //document.body.style.backgroundImage = `linear-gradient(to bottom, rgb(${r},${g},${b}) , rgb(${R},${G},${B}));`
-    //document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    var rr = Math.trunc(Math.pow(r * R, 0.5));
+    var gg = Math.trunc(Math.pow(g * G, 0.5));
+    var bb = Math.trunc(Math.pow(b * B, 0.5));
+    $("body").css({
+        'background-image': `linear-gradient(to top,  rgb(${r},${g},${b}) 1%, rgb(${R},${G},${B}) 100%)`
+    });
     document.getElementById("me").style.backgroundColor = `rgb(${rr},${gg},${bb})`;
 }
 setInterval(function() {
     changeBackground();
-},117);
+}, 117);
 
-function openInNewTabWinBrowser(url) {
-  var win = window.open(url, '_blank');
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
 }
 
 $(".fa-github").click(function() {
-    openInNewTabWinBrowser("https://www.github.com/Ambyjkl");
+    openInNewTab("https://www.github.com/Ambyjkl");
 });
 $(".fa-facebook-square").click(function() {
-    openInNewTabWinBrowser("https://www.facebook.com/ambareesh.balaji");
+    openInNewTab("https://www.facebook.com/ambareesh.balaji");
+});
+$(".fa-twitter").click(function() {
+    openInNewTab("https://twitter.com/AmbareeshBalaji");
 });
 $(".fa-linkedin").click(function() {
-    openInNewTabWinBrowser("https://www.linkedin.com/in/ambareesh-b-ba2978129");
+    openInNewTab("https://www.linkedin.com/in/ambareesh-b-ba2978129");
 });
 $(".fa-envelope-square").click(function() {
-    openInNewTabWinBrowser("mailto:ambareeshbalaji@gmail.com");
+    openInNewTab("mailto:ambareeshbalaji@gmail.com");
 });
 $(".fa-file-pdf-o").click(function() {
-    openInNewTabWinBrowser("resume.pdf");
+    openInNewTab("resume.pdf");
 });
